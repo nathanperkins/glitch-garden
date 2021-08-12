4b5b487ff7936660beb06ef45b84b7e1f863f442
Nathan Perkins <nperkins487@gmail.com>
2021-08-11 22:40:09 -0700

    Implement projectile damage

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,15 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour
+public class Attacker : MonoBehaviour, IHealth
 {
-    [Range(0f, 5f)]
-    float currentSpeed;
+    [SerializeField] int startingHealth;
+
+    #region State
+    [Header("State")]
+    [SerializeField] float currentSpeed;
+    [SerializeField] int currentHealth;
+    #endregion
 
     void Start()
     {
-        currentSpeed = 0f;
+        currentSpeed = 0;
+        currentHealth = startingHealth;
     }
 
     // Update is called once per frame
@@ -18,7 +22,24 @@ public class Attacker : MonoBehaviour
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
     }
 
-    public void SetMovementSpeed(float speed) {
+    public void SetMovementSpeed(float speed)
+    {
         currentSpeed = speed;
+    }
+
+    public void DealDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log($"{name} took {damage} damage; now has {currentHealth} health");
+        if (currentHealth <= 0)
+        {
+            Die();
+		}
+	}
+
+    void Die()
+    {
+        Debug.Log($"{name} is dead");
+        Destroy(gameObject);
 	}
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private int damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<IHealth>()?.DealDamage(damage);
+    }
+}
