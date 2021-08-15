5583145e26e79d1ef18750835538c94986830195
Nathan Perkins <nperkins487@gmail.com>
2021-08-14 22:53:54 -0700

    Make shooter aware of whether there are attackers in line

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -10,7 +10,53 @@ public class Shooter : MonoBehaviour
     [Range(0.5f, 5)]
     [SerializeField] float projectileVelocity; 
     [Range(0, 720)]
-    [SerializeField] float projectileRotation; 
+    [SerializeField] float projectileRotation;
+
+    Spawner spawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot pew pew");
+            // TODO: Change animation state to Shooting.
+		}
+        else
+        {
+            Debug.Log("sit and wait");
+            // TODO: Change animation state to Idle.
+		}
+    }
+
+    private void SetLaneSpawner()
+    {
+        float myYPos = transform.position.y;
+        foreach (var spawner in FindObjectsOfType<Spawner>())
+        {
+            float spawnerYPos = spawner.transform.position.y;
+            if (Mathf.Approximately(myYPos, spawnerYPos))
+            {
+                this.spawner = spawner;
+                return;
+			}
+		}
+        throw new System.Exception($"No spawner found in {name}'s lane");
+	}
+
+    private bool IsAttackerInLane()
+    { 
+        if (spawner == null)
+        {
+            return false;
+		}
+
+        return spawner.GetComponentInChildren<Attacker>() != null;
+	}
 
     public void Fire()
 	{
