1471e7a1aa5f4aad587d283dd32a763aba8bf288
Nathan Perkins <nperkins487@gmail.com>
2021-08-14 23:10:51 -0700

    Shoot from cactus only when attackers in lane; only place one defender per lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,29 +8,22 @@ public class Shooter : MonoBehaviour
     [Header("Projectile")]
     [SerializeField] GameObject gun;
     [Range(0.5f, 5)]
-    [SerializeField] float projectileVelocity; 
+    [SerializeField] float projectileVelocity;
     [Range(0, 720)]
     [SerializeField] float projectileRotation;
 
+    Animator animator;
     Spawner spawner;
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         SetLaneSpawner();
     }
 
     private void Update()
     {
-        if (IsAttackerInLane())
-        {
-            Debug.Log("shoot pew pew");
-            // TODO: Change animation state to Shooting.
-		}
-        else
-        {
-            Debug.Log("sit and wait");
-            // TODO: Change animation state to Idle.
-		}
+        animator.SetBool("IsAttacking", IsAttackerInLane());
     }
 
     private void SetLaneSpawner()
@@ -43,32 +36,32 @@ public class Shooter : MonoBehaviour
             {
                 this.spawner = spawner;
                 return;
-			}
-		}
+            }
+        }
         throw new System.Exception($"No spawner found in {name}'s lane");
-	}
+    }
 
     private bool IsAttackerInLane()
-    { 
+    {
         if (spawner == null)
         {
             return false;
-		}
+        }
 
         return spawner.GetComponentInChildren<Attacker>() != null;
-	}
+    }
 
     public void Fire()
-	{
+    {
         // Debug.Log("Fire!");
         var projectile = Instantiate(
             projectilePrefab,
             gun.transform.position,
             Quaternion.identity
-		);
+        );
 
         var body = projectile.GetComponent<Rigidbody2D>();
         body.velocity = Vector2.right * projectileVelocity;
         body.angularVelocity = -projectileRotation;
-	}
+    }
 }
